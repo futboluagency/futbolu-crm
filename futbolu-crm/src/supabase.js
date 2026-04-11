b6afbc75404b97981a028f84b02596f31cb9ffa8
futboluagency <futboluagency@gmail.com>
2026-04-11 19:59:04 +0100

    Change Supabase key to a new value
    Updated Supabase key to a new secure value.

--- futbolu-crm/src/supabase.js
+++ futbolu-crm/src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://jjgtgkqmxnlxkshykdxv.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_I1XgUPcgyMP-ujd9Jicv4w_H7UiQ_Qr';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpqZ3Rna3FteG5seGtzaHlrZHh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MTY4NDUsImV4cCI6MjA5MTQ5Mjg0NX0.jH91dzKwwwaVLZbRyEoBlyIEPjZn5oX_NDIGYGULRAs';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);

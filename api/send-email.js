752ae3bc88d3ebcb720c5073bb0d65dfa59eafbe
futboluagency <futboluagency@gmail.com>
2026-04-12 23:35:54 +0100

    Add CORS support to send-email endpoint
    Added CORS headers to the email sending endpoint.

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
 
 module.exports = async (req, res) => {
+  // CORS headers
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
